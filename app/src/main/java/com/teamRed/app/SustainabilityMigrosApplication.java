f390aa647d87ca694dbe547edb3704ea4c624d5e
Lovnesh <lovneshbhardwaj@gmail.com>
2021-09-24 21:40:40 +0200

    Added Spring Project
    added spring startup

---
+++ app/src/main/java/com/teamRed/app/SustainabilityMigrosApplication.java
@@ -0,0 +1,13 @@
+package com.teamRed.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustainabilityMigrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustainabilityMigrosApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/teamRed/app/SustainabilityMigrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamRed.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustainabilityMigrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
