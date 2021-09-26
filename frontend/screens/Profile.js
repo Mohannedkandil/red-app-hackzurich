3469f9aae616d484d2479de0f906af556bbb7304
Adrian Mitter <adrian.mitter@tum.de>
2021-09-26 06:04:49 +0200

    Update profile UI

--- frontend/screens/Profile.js
+++ frontend/screens/Profile.js
@@ -1,82 +1,55 @@
 import React from "react";
 import {ImageBackground, StyleSheet, View} from 'react-native';
 
-import { Text, Avatar, HStack, VStack, Progress, Center, Container, Image, Box, Stack, Heading, Circle, ZStack, Square } from "native-base";
-import { alignSelf, flex, flexDirection } from "styled-system";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {Center, Circle, Container, Flex, Heading, Image, Pressable, Progress, Text, VStack, ZStack} from "native-base";
 
+const passportData = [
+    {name: "Gundeldingen", city: "Basel", image: require('../assets/stores/migros1.png')},
+    {name: "MPark", city: "Basel", image: require('../assets/stores/migros2.png')},
+    {name: "Altmarkt", city: "Zürich", image: require('../assets/stores/migros4.png')},
+    {name: "Bahnhof", city: "Basel", image: require('../assets/stores/migros3.png')},
+]
 
-export default function Profile() {
+export default function Profile({navigation}) {
     return (
         <View flex={1}>
             <ImageBackground source={require('../assets/background.png')} style={{flex: 1, resizeMode: 'cover', justifyContent: 'center'}}>
-            <Text fontWeight="bold" color="black" alignSelf="center" marginTop={10} fontSize={30}>SuperBuyer5</Text>
-            <Container style={{ alignSelf: "center", height: "34%" }}>
-                <ZStack alignSelf="center">
-                    <Circle size={200} bg="#ED702D" alignSelf="center" marginTop={12}>
-                        <Text>Level</Text>
+                <Text fontWeight="bold" color="black" alignSelf="center" fontSize={30}>SuperBuyer5</Text>
+                <Container style={{alignSelf: "center", height: "34%"}}>
+                    <ZStack alignSelf="center">
+                        <Circle size={200} bg="#ED702D" alignSelf="center" marginTop={12}/>
+                        <Image
+                            style={{alignSelf: 'center'}}
+                            size="2xl"
+                            source={require('../assets/avatar.png')}
+                            alt="avatar"
+                        />
+                    </ZStack>
+                </Container>
+                <Text textAlign="center" bold fontSize="18px">Level 12</Text>
 
-                    </Circle>
-
-                    <Image
-                        style={{ alignSelf: 'center' }}
-                        size="2xl"
-                        source={{
-                            uri: "https://i.ibb.co/BGQsgfW/emoji.png",
-                        }}
-                    />
-                </ZStack>
-            </Container>
-            <Box alignItems="center">
-                <Text fontSize={14} fontWeight={700}>Level</Text>
-                <Text fontSize={30} fontWeight="bold">12</Text>
-
-            </Box>
-            <Box wdith="220">
-                <Text fontSize={14} fontWeight="bold" alignSelf="center">GREEN HERO!</Text>
-                <Progress value={30} mx="2" />
-                <Text fontSize={14} fontWeight="bold" alignSelf="center">135'225 / 300'00 XP</Text>
-            </Box>
-
-            <HStack space={5} justifyContent="center" mt={7}>
-                <VStack space={2}>
-                    <Image
-                        source={
-                            require('../assets/badges/badge_1.png')
-                        }
-                        size="lg"
-                    />
-                    <Text fontSize={14} fontWeight="bold" textAlign="center">Gundeldingen {"\n"} Basel</Text>
-
+                <VStack mt="25px" space={2}>
+                    <Text fontSize={14} fontWeight="bold" alignSelf="center">GREEN HERO</Text>
+                    <Center><Progress value={35} width="200px"/></Center>
+                    <Text fontSize={14} fontWeight="bold" alignSelf="center">135225 / 30000 XP</Text>
                 </VStack>
 
-                <VStack space={2}>
-                    <Image
-                        source={
-                            require('../assets/badges/badge_1.png')
-                        }
-                        size="lg"
-                    />
-                    <Text fontSize={14} fontWeight="bold" textAlign="center">Gundeldingen {"\n"} Basel</Text>
+                <Heading mt="20px" mb="3" ml={3}>Passport</Heading>
+                <Flex direction="row" wrap="wrap" justifyContent="space-between">
+                    {passportData.slice(0, 3).map((x, index) => <Flex key={index} direction="column" alignItems="center" ml="10px"
+                                                                      mr="10px">
+                        <Image source={x.image} size="45px" alt="Store 1"/>
+                        <Text fontSize={14} fontWeight="bold" mt="4px" textAlign="center">{x.name} {"\n"} {x.city}</Text>
+                    </Flex>)}
+                </Flex>
 
-                </VStack>
-                <VStack space={2}>
-                    <Image
-                        source={
-                            require('../assets/badges/badge_1.png')
-                        }
-                        size="lg"
-                    />
-                    <Text fontSize={14} fontWeight="bold" textAlign="center">Gundeldingen {"\n"} Basel</Text>
-
-                </VStack>
-
-
-
-
-            </HStack>
+                <Center mt="40px">
+                    <Pressable onPress={() => navigation.navigate("Home")}>
+                        <Image size="55px" source={require('../assets/menu/icon-close.png')} alt="Back to home screen"/>
+                    </Pressable>
+                </Center>
             </ImageBackground>
-        </View >
+        </View>
     );
 }
 
