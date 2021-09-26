80f8546a14ab7275825e0e80f71e26b635663126
Adrian Mitter <adrian.mitter@tum.de>
2021-09-26 03:22:11 +0200

    Update daily challenges

--- frontend/components/ChallengeTabView.js
+++ frontend/components/ChallengeTabView.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Box, Circle, HStack, Image, Progress, Text, VStack} from "native-base";
+import {Box, Center, Circle, HStack, Image, Progress, Text, VStack} from "native-base";
 import {Animated, Dimensions, Pressable, StatusBar} from "react-native";
 import {SceneMap, TabView} from "react-native-tab-view";
 
@@ -16,7 +16,7 @@ const monthlyChallenges = [
     {name: "Scan 100 items", xp: 2000, progress: 85},
 ]
 
-const DailyTab = () => <Image source={require('../assets/daily-challenges.png')} alt="daily"/>;
+const DailyTab = () => <Center><Image size="225px" source={require('../assets/daily-challenges.png')} alt="daily"/></Center>;
 
 const WeeklyTab = () => <VStack space={1.5} mt="1.5">{dailyChallenges.map((elem, index) => {
     return <HStack key={index} space={3} justifyContent="space-between" alignItems="center" rounded="10px" padding="2" paddingTop="1.5"
